18de568a80d18d5cddec0a31f3ceae56daeb70e3
Ankush Raj <ankushr.ug23.ec@nitp.ac.in>
2024-03-20 18:36:29 +0530

    Initial commit

---
+++ container/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}): super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('Flutter Container'),
+      ),
+      body:Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+          child: Text('Hello Ankush Raj!!'),
+        ),
+      )
+      );
+  }
+}
